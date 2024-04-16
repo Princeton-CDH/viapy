4c4e105a2b9ad084ee7743f0f079a22006d5c266
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-16 18:19:51 -0400

    Migrate from setup.py to pyproject.toml

--- viapy/__init__.py
+++ viapy/__init__.py
@@ -1,7 +1,1 @@
-__version_info__ = (0, 3, 0, 'dev')
-
-
-# Dot-connect all but the last. Last is dash-connected if not None.
-__version__ = '.'.join([str(i) for i in __version_info__[:-1]])
-if __version_info__[-1] is not None:
-    __version__ += ('-%s' % (__version_info__[-1],))
+__version__ = "0.3.0.dev0"
