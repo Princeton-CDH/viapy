a49bce51682abf3a34450fa46f2cc0e88ba58404
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-17 12:56:12 -0400

    Update docs/sphinx configuration

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -25,9 +25,9 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("."))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'docsettings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "docsettings"
 django.setup()
 
 from viapy import __version__
@@ -43,29 +43,29 @@ from viapy import __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'viapy'
-copyright = '2017, CDH @ Princeton'
-author = 'CDH @ Princeton'
+project = "viapy"
+copyright = "2017, CDH @ Princeton"
+author = "CDH @ Princeton"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -81,15 +81,15 @@ release = __version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -100,7 +100,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -108,18 +108,19 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 html_theme_options = {
-    'description': 'VIAF via Python',
-    'github_user': 'Princeton-CDH',
-    'github_repo': 'viapy',
-    'travis_button': True,
-    'codecov_button': True,
+    "description": "VIAF via Python",
+    "github_user": "Princeton-CDH",
+    "github_repo": "viapy",
+    "travis_button": True,
+    "codecov_button": True,
 }
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# currently unused
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -127,12 +128,12 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
+    "**": [
+        "about.html",
+        "navigation.html",
         # 'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'sidebar_footer.html',
+        "searchbox.html",
+        "sidebar_footer.html",
     ]
 }
 
@@ -140,7 +141,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'viapydoc'
+htmlhelp_basename = "viapydoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -149,15 +150,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -167,8 +165,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'viapy.tex', 'viapy Documentation',
-     'CDH @ Princeton', 'manual'),
+    (master_doc, "viapy.tex", "viapy Documentation", "CDH @ Princeton", "manual"),
 ]
 
 
@@ -176,10 +173,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'viapy', 'viapy Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "viapy", "viapy Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -188,11 +182,23 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'viapy', 'viapy Documentation',
-     author, 'viapy', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "viapy",
+        "viapy Documentation",
+        author,
+        "viapy",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "django": (
+        "http://docs.djangoproject.com/en/dev/",
+        "http://docs.djangoproject.com/en/dev/_objects/",
+    ),
+}
