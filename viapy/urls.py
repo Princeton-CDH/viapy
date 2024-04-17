964c873d1ff37a0f5837d127821f2f36581896ae
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-17 12:07:08 -0400

    Update to remove deprecated django url syntax

--- viapy/urls.py
+++ viapy/urls.py
@@ -1,15 +1,23 @@
-from django.conf.urls import url
+from django.urls import path
 
 from viapy.views import ViafLookup, ViafSearch
 
 
-app_name = 'viapy'
+app_name = "viapy"
 
 urlpatterns = [
-    url(r'^suggest/$', ViafLookup.as_view(), name='suggest'),
-    url(r'^suggest/person/$', ViafLookup.as_view(),
-        {'nametype': 'personal'}, name='person-suggest'),
-    url(r'^search/$', ViafSearch.as_view(), name='search'),
-    url(r'^search/person/$', ViafSearch.as_view(),
-        {'nametype': 'personal'}, name='person-search'),
+    path("suggest/", ViafLookup.as_view(), name="suggest"),
+    path(
+        "suggest/person/",
+        ViafLookup.as_view(),
+        {"nametype": "personal"},
+        name="person-suggest",
+    ),
+    path("search/", ViafSearch.as_view(), name="search"),
+    path(
+        "search/person/",
+        ViafSearch.as_view(),
+        {"nametype": "personal"},
+        name="person-search",
+    ),
 ]
